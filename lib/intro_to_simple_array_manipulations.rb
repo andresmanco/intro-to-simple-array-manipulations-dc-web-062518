341bef36bfde7b694b8b57b976ac5eb862a686ae
Andres Manco <andresmanco@gmail.com>
2018-06-19 08:34:07 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,4 +22,5 @@ def shift_with_args(ice_cream_brands)
   brands_removed = ice_cream_brands.shift(2)
 end
 
-def 
+def using_concat(my_favorite_things, more_favs)
+  all_my_favs = my_favorite_things + more_favs
