3a94e60c77479c01568c4b2dd634a1da31bf8f9d
Andres Manco <andresmanco@gmail.com>
2018-06-19 08:25:57 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,11 @@
+def using_push(countries_in_western_africa, next_country)
+  updated_array = countries_in_western_africa.push(next_country)
+end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  updated_array = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+end
+
+def using_pop(great_hits_of_the_nineties)
+  deleted_string = great_hits_of_the_nineties.pop
+end
